4b3bab064fc4433818bea3937fc4710eda331131
Perlman <perlman20201026@gmail.com>
2022-05-26 10:11:20 +0800

    nothing

---
+++ GithubUpdataTest/GithubUpdataTest.cpp
@@ -0,0 +1,13 @@
+// GithubUpdataTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "opencv.hpp"
+
+using namespace std;
+int main()
+{
+	cout << "hellow nidi shijie,haha" << endl;
+    return 0;
+}
+
